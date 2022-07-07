6d21c617ad9199cd50b3b922eccb7e70dac834d5
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-07-07 14:13:09 +0300

    add register api

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -169,7 +169,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
    
     "http://localhost:4200",
-    "http://127.0.0.1:8000",
+    # "http://127.0.0.1:8000",
     'https://backendjw.herokuapp.com/',
 ]
 AUTH_USER_MODEL='app.User'
